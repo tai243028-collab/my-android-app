188d3d831f1026a24b5e79cf464524236f0dda44
tai243028-collab <tai243028@gmail.com>
2026-09-16 18:27:17 +0700

    Create MainActivity class for the app

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
